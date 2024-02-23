8f380777d07de3c486927fb04d33fa316f5b3314
Orkesha <76640405+Orkesha@users.noreply.github.com>
2024-02-23 19:51:21 +0600

    Update nano_car.ino

--- nano_car/nano_car.ino
+++ nano_car/nano_car.ino
@@ -1,98 +1,116 @@
+#include <SoftwareSerial.h>
+SoftwareSerial ser(2, 3);
 #include <Servo.h>
-Servo servo;
+Servo srv1;
+Servo srv2;
+Servo srv3;
+Servo srv4;
 
 
-#define min1
-#define min2
-#define min3
-#define min4
+#define m1 4
+#define m2 5
+#define m3 6
+#define m4 7
 
-#define atri
-#define rela
-#define pomo
-#define pomi
-#define serv
-
-#define mou1
-#define mou2
-#define mou3
-#define mou4
-
-#define led1
-#define led2
-#define sen1
-#define sen2
-#define sen3
+#define ser1 8
+#define ser2 9
+#define ser3 10
+#define ser4 11
 
 
-
-void setup(){
-  pinMode(min1, INPUT);
-  pinMode(min2, INPUT);
-  pinMode(min3, INPUT);
-  pinMode(min4, INPUT);
-
-  pinMode(atri, INPUT);
-  pinMode(rela, OUTPUT);
-  pinMode(pomo, OUTPUT);
-  pinMode(pomi, INPUT);
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(sen1, INPUT);
-  pinMode(sen2, INPUT);
-  pinMode(sen3, INPUT);
-  
-
-  pinMode(mou1, OUTPUT);
-  pinMode(mou2, OUTPUT);
-  pinMode(mou3, OUTPUT);
-  pinMode(mou4, OUTPUT);
-  
+void setup() {
+srv1.attach(ser1);
+srv2.attach(ser2);
+srv3.attach(ser3);
+srv4.attach(ser4);
+srv1.write(0);
+srv2.write(0);
+srv3.write(0);
+srv4.write(0);
+ser.begin(115200);
+Serial.begin(115200);
+pinMode(m1, OUTPUT);
+pinMode(m2, OUTPUT);
+pinMode(m3, OUTPUT);
+pinMode(m4, OUTPUT);
 }
 
-unsigned long dela = 0;
-int state = 1;
-
-void loop(){
-if(millis() - dela > 500){
-  state +=1;
-  if(state > 8){
-    state = 1;
+void loop() {
+  if(ser.available()){
+    String data = ser.readStringUntil('\n');
+    char comm = data.charAt(0);
+    int val = data.substring(1).toInt();
+  switch(comm){
+    case 'F':
+    if(val == 0){
+      digitalWrite(m1, 1);
+      digitalWrite(m2, 1);
+      digitalWrite(m3, 0);
+      digitalWrite(m4, 0);
+      delay(1000);
+      digitalWrite(m1, 0);
+      digitalWrite(m2, 0);
+    }
+    else{
+      digitalWrite(m1, 1);
+      digitalWrite(m2, 1);
+      digitalWrite(m3, 0);
+      digitalWrite(m4, 0);
+    }
+    break;
+    case 'R':
+      digitalWrite(m1, 1);
+      digitalWrite(m2, 0);
+      digitalWrite(m3, 1);
+      digitalWrite(m4, 0);
+      delay(1000);
+      digitalWrite(m1, 0);
+      digitalWrite(m3, 0);      
+    break;
+    case 'L':
+      digitalWrite(m1, 0);
+      digitalWrite(m2, 1);
+      digitalWrite(m3, 0);
+      digitalWrite(m4, 1);
+      delay(1000);
+      digitalWrite(m2, 0);
+      digitalWrite(m4, 0);      
+    break;
+    case 'B':
+    if(val == 0){
+      digitalWrite(m1, 0);
+      digitalWrite(m2, 0);
+      digitalWrite(m3, 1);
+      digitalWrite(m4, 1);
+      delay(1000);
+      digitalWrite(m3, 0);
+      digitalWrite(m4, 0);
+    }
+    else{
+      digitalWrite(m1, 0);
+      digitalWrite(m2, 0);
+      digitalWrite(m3, 1);
+      digitalWrite(m4, 1);
+    }
+    break;
+    case 'S':
+      digitalWrite(m1, 0);
+      digitalWrite(m2, 0);
+      digitalWrite(m3, 0);
+      digitalWrite(m4, 0);
+    break;
+    case 'Z':
+    srv1.write(val);
+    break;
+    case 'X':
+    srv2.write(val);
+    break;
+    case 'C':
+    srv3.write(val);
+    break;
+    case 'V':
+    srv4.write(val);
+    break;
   }
-  if(state == 1 or state == 3){
-    digitalWrite(led1, 0);
-    digitalWrite(led2, 1);
   }
-  if(state == 2 or state == 4 or state == 6 or state == 8){
-    digitalWrite(led1, 0);
-    digitalWrite(led2, 0);
-  }
-  if(state == 5 or state == 7){
-    digitalWrite(led1, 1);
-    digitalWrite(led2, 0);
-  }
-}
-if(digitalRead(atri) == 0){
-  digitalWrite(mou1, digitalRead(min1));
-  digitalWrite(mou2, digitalRead(min2));
-  digitalWrite(mou3, digitalRead(min3));
-  digitalWrite(mou4, digitalRead(min4));  
-
-  digitalWrite(pomo, digitalRead(pomi));
-  digitalWrite(rela, 0);
-}
-if(digitalRead(atri) == 1){
-int sensor1 = analogRead(sen1);
-int sensor2 = analogRead(sen2);
-int sensor3 = analogRead(sen3);
-if(sensor1 > sensor2 and sensor1 > sensor3){
-  servo.write();
-}
-if(sensor2 > sensor1 and sensor2 > sensor3){
-  servo.write();
-}
-if(sensor3 > sensor2 and sensor3 > sensor1){
-  servo.write();
-}
-}
 }
