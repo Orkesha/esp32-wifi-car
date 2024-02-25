c2038fb334a6d56a844c69e5879507774af613e0
Orkesha <76640405+Orkesha@users.noreply.github.com>
2024-02-25 18:24:07 +0600

    added smooth rotation of the servo

--- nano_car/nano_car.ino
+++ nano_car/nano_car.ino
@@ -6,6 +6,7 @@ Servo srv2;
 Servo srv3;
 Servo srv4;
 
+int last[4] = {90, 0, 180, 0};
 
 #define m1 4
 #define m2 5
@@ -23,10 +24,10 @@ srv1.attach(ser1);
 srv2.attach(ser2);
 srv3.attach(ser3);
 srv4.attach(ser4);
-srv1.write(0);
-srv2.write(0);
-srv3.write(0);
-srv4.write(0);
+srv1.write(last[0]);
+srv2.write(last[1]);
+srv3.write(last[2]);
+srv4.write(last[3]);
 ser.begin(115200);
 Serial.begin(115200);
 pinMode(m1, OUTPUT);
@@ -44,52 +45,52 @@ void loop() {
     case 'F':
     if(val == 0){
       digitalWrite(m1, 1);
-      digitalWrite(m2, 1);
-      digitalWrite(m3, 0);
+      digitalWrite(m2, 0);
+      digitalWrite(m3, 1);
       digitalWrite(m4, 0);
       delay(1000);
       digitalWrite(m1, 0);
-      digitalWrite(m2, 0);
+      digitalWrite(m3, 0);
     }
     else{
       digitalWrite(m1, 1);
-      digitalWrite(m2, 1);
-      digitalWrite(m3, 0);
+      digitalWrite(m2, 0);
+      digitalWrite(m3, 1);
       digitalWrite(m4, 0);
     }
     break;
     case 'R':
       digitalWrite(m1, 1);
       digitalWrite(m2, 0);
+      digitalWrite(m3, 0);
+      digitalWrite(m4, 1);
+      delay(1000);
+      digitalWrite(m1, 0);
+      digitalWrite(m4, 0);      
+    break;
+    case 'L':
+      digitalWrite(m1, 0);
+      digitalWrite(m2, 1);
       digitalWrite(m3, 1);
       digitalWrite(m4, 0);
       delay(1000);
-      digitalWrite(m1, 0);
+      digitalWrite(m2, 0);
       digitalWrite(m3, 0);      
     break;
-    case 'L':
+    case 'B':
+    if(val == 0){
       digitalWrite(m1, 0);
       digitalWrite(m2, 1);
       digitalWrite(m3, 0);
       digitalWrite(m4, 1);
       delay(1000);
       digitalWrite(m2, 0);
-      digitalWrite(m4, 0);      
-    break;
-    case 'B':
-    if(val == 0){
-      digitalWrite(m1, 0);
-      digitalWrite(m2, 0);
-      digitalWrite(m3, 1);
-      digitalWrite(m4, 1);
-      delay(1000);
-      digitalWrite(m3, 0);
       digitalWrite(m4, 0);
     }
     else{
       digitalWrite(m1, 0);
-      digitalWrite(m2, 0);
-      digitalWrite(m3, 1);
+      digitalWrite(m2, 1);
+      digitalWrite(m3, 0);
       digitalWrite(m4, 1);
     }
     break;
@@ -100,16 +101,68 @@ void loop() {
       digitalWrite(m4, 0);
     break;
     case 'Z':
-    srv1.write(val);
+    val = map(val, 0, 100, 0, 180);
+    if(val > last[0]){
+    for(int i = last[0]; i<val+1; i++){
+    srv1.write(i);
+    delay(10);  
+    }
+    }
+    else{
+    for(int i = last[0]; i>val+1; i--){
+    srv1.write(i);
+    delay(10);  
+    }
+    }
+    last[0] = val;
     break;
     case 'X':
-    srv2.write(val);
+    val = map(val, 0, 100, 0, 180);
+    if(val > last[1]){
+    for(int i = last[1]; i<val+1; i++){
+    srv2.write(i);
+    delay(10);  
+    }
+    }
+    else{
+    for(int i = last[1]; i>val+1; i--){
+    srv2.write(i);
+    delay(10);  
+    }
+    }
+    last[1] = val;
     break;
     case 'C':
-    srv3.write(val);
+    val = map(val, 0, 100, 0, 180);
+    if(val > last[2]){
+    for(int i = last[2]; i<val+1; i++){
+    srv3.write(i);
+    delay(10);  
+    }
+    }
+    else{
+    for(int i = last[2]; i>val+1; i--){
+    srv3.write(i);
+    delay(10);  
+    }
+    }
+    last[2] = val;
     break;
     case 'V':
-    srv4.write(val);
+    val = map(val, 0, 100, 0, 180);
+    if(val > last[3]){
+    for(int i = last[3]; i<val+1; i++){
+    srv4.write(i);
+    delay(10);  
+    }
+    }
+    else{
+    for(int i = last[3]; i>val+1; i--){
+    srv4.write(i);
+    delay(10);  
+    }
+    }
+    last[3] = val;
     break;
   }
   }
